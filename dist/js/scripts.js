04d575ed1894165072f85b45395e65ce3d15262b
Muhammad Abdullah <imuhammadabdullah@outlook.com>
2024-11-14 23:22:18 +0500

    check this

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Agency v7.0.12 (https://github.com/imabdullah/bitbyte)
+* Start Bootstrap - Agency v7.0.12 (https://imabdullah.github.io/bitbyte)
 * Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
 */
